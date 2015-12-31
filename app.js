1a308d6a21806fd3a51e442030a76aaf78ed927b
Armando Amador <arm.amador@gmail.com>
2015-12-30 23:07:45 -0500

    setup code quality and code style enforcements on project
     - jshint and jscs

--- app.js
+++ app.js
@@ -7,12 +7,11 @@ var port = 5000;
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
-
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.send('Hello World');
 });
 
-app.get('/books', function(req, res){
+app.get('/books', function(req, res) {
   res.send('Hello Books');
 });
 
